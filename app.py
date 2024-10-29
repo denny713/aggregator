17937d6f51f6267e4e85ea7e906e6fd18fff002d
Denny Afrizal <denny.afrizal713@gmail.com>
2024-10-29 23:06:58 +0700

    Detik.som Scrapping

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template, request, jsonify
 
 from scrape.acm import scrape_acm
+from scrape.detik import scrape_detik
 from scrape.wiki import wiki_scrap
 
 app = Flask(__name__, static_folder='assets', template_folder='pages')
@@ -22,10 +23,57 @@ def scrap():
     search = req_data.get('search')
 
     match module:
-        case "wikipedia":
-            data = wiki_scrap(typ, search)
+        # Social media
+        case "facebook":
+            data = {"data": []}
+        case "instagram":
+            data = {"data": []}
+        case "youtube":
+            data = {"data": []}
+        case "twitter":
+            data = {"data": []}
+        case "tiktok":
+            data = {"data": []}
+
+        # QA crowdsourcing
+        case "stackoverflow":
+            data = {"data": []}
+
+        # Application marketplace
+        case "playstore":
+            data = {"data": []}
+        case "appstore":
+            data = {"data": []}
+
+        # Academic literature
+        case "ieee":
+            data = {"data": []}
         case "acm":
             data = scrape_acm(typ, search)
+        case "springer":
+            data = {"data": []}
+        case "scholar":
+            data = {"data": []}
+        case "bookonline":
+            data = {"data": []}
+        case "sciencedirect":
+            data = {"data": []}
+        case "wikipedia":
+            data = wiki_scrap(typ, search)
+
+        # Indonesia marketplace
+        case "tokopedia":
+            data = {"data": []}
+        case "shopee":
+            data = {"data": []}
+        case "bukalapak":
+            data = {"data": []}
+
+        # Indonesia news
+        case "detik":
+            data = scrape_detik(search)
+
+        # Others
         case _:
             data = {"data": []}
 
